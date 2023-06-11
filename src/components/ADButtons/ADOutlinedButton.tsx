6aa05e9dabeea773aa31d70969d83bbbf5a2bb83
ProjectSkyapple <36686926+ProjectSkyapple@users.noreply.github.com>
2023-06-11 08:24:46 -0500

    New ADOutlinedButton

---
+++ src/components/ADButtons/ADOutlinedButton.tsx
@@ -0,0 +1,39 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { Pressable } from "react-native";
+import buttonStyles from "../styles/buttonStyles";
+import { useState } from "react";
+import ADText from "../ADText/ADText";
+
+interface ADOutlinedButtonProps {
+  text: string;
+  onPress?: () => void;
+}
+
+const ADOutlinedButton = (props: ADOutlinedButtonProps) => {
+  const [color, setColor] = useState("#FFF0");
+
+  return (
+    <Pressable
+      onPressIn={() => {
+        setColor("#DDD");
+      }}
+      onPressOut={() => {
+        setColor("#FFF0");
+      }}
+      onPress={props.onPress}
+      style={{ width: "100%" }}
+    >
+      <LinearGradient
+        colors={[color, color]}
+        style={[
+          buttonStyles.baseButton,
+          { borderColor: "#000", borderWidth: 1.5 },
+        ]}
+      >
+        <ADText style={[buttonStyles.baseButtonText]}>{props.text}</ADText>
+      </LinearGradient>
+    </Pressable>
+  );
+};
+
+export default ADOutlinedButton;
