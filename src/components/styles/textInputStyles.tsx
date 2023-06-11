f57cd63a3a1c42c260da6ffbe505efb83fa7ac47
ProjectSkyapple <36686926+ProjectSkyapple@users.noreply.github.com>
2023-06-11 09:45:10 -0500

    Create textInputStyles.tsx

---
+++ src/components/styles/textInputStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const textInputStyles = StyleSheet.create({
+  baseTextInput: {
+    backgroundColor: "#DDD",
+    borderRadius: 12,
+    padding: 12,
+  },
+  baseTextInputView: {
+    rowGap: 6,
+  },
+});
+
+export default textInputStyles;
