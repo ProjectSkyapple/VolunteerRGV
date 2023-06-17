216b5357c8fa245eba349f2b2e9913245ebe01ee
AvgeekyDev <36686926+ProjectSkyapple@users.noreply.github.com>
2023-06-17 09:15:45 -0700

    Fix env var config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,21 @@
 // Import functions from SDKs and APIs needed
 import { initializeApp } from "firebase/app";
 import { initializeAuth, reactNativeLocalPersistence } from "firebase/auth";
-import { Constants } from "expo-constants";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 
 // Firebase configuration (web)
 const firebaseConfig = {
-  apiKey: Constants.manifest?.extra?.firebaseApiKey,
-  authDomain: Constants.manifest?.extra?.firebaseProjectId + ".firebaseapp.com",
-  projectId: Constants.manifest?.extra?.firebaseProjectId,
-  storageBucket: Constants.manifest?.extra?.firebaseProjectId + ".appspot.com",
-  messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
-  appId: Constants.manifest?.extra?.firebaseAppId,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_PROJECT_ID + ".firebaseapp.com",
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_PROJECT_ID + ".appspot.com",
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
