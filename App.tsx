972949a230734ca1432f26f6b18ea8400a485c5a
AvgeekyDev <36686926+ProjectSkyapple@users.noreply.github.com>
2023-06-17 06:27:47 -0700

    Update nav stack

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import { View } from "react-native";
 import AuthenticationScreen from "./src/components/screens/AuthenticationScreen";
 import DetailsScreen from "./src/components/screens/DetailsScreen";
 import HomeScreen from "./src/components/screens/HomeScreen";
-import VerifyCodeScreen from "./src/components/screens/VerifyCodeScreen";
 import StatusBarArea from "./src/components/global/StatusBarArea";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Airtable from "airtable";
 
 const Stack = createNativeStackNavigator();
 
@@ -29,7 +29,6 @@ export default function App() {
             name="Authentication"
             component={AuthenticationScreen}
           />
-          <Stack.Screen name="Verify Code" component={VerifyCodeScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
